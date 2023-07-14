b1237fb24681360de61a9f4b729e98b2e764ec07
Michele de Barros Santos <michele.fenix.17@gmail.com>
2023-07-13 22:27:05 -0300

    gerador de senha

--- funcoes.py
+++
@@ -1,8 +0,0 @@
-import secrets
-import string
-
-# Gerar senhas com caracteres hexadecimais e caracteres especiais de pontuação
-itens = (string.hexdigits)
-senha = [secrets.choice(itens) for i in range(32)]
-senha = ''.join(senha)  # Tira a separação '', da lista
-print(senha)
